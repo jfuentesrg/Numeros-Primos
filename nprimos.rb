5e470dd2976d00a16e8d03091f12ae003344d351
javier fuentes rivera galvan <jfuentesrg@gmail.com>
2012-04-10 11:53:39 -0500

    'para24'

--- nprimos.rb
+++ nprimos.rb
@@ -1,7 +1,23 @@
 class NumerosPrimos
 
-def self.generar numero 
+	def calcular(numero)	
+		a = 0 
+	
+		factor = []		
+        while numero > 1 do         	
+	        for num in 2..numero 
+				while numero % num ==0 do
+					numero = numero / num
+					factor[a] = num
+					a += 1
 
-end
+				end
+			end	
+        end
+       
+		factor.sort
+	end
+
+
 
 end
--- nprimos_spec.rb
+++ nprimos_spec.rb
@@ -1,1 +1,12 @@
-sd288 => [2,2,2,2,2,3,3]
+require "./nprimos.rb"
+describe nprimos do
+#Test	
+	it "primos de 24" do
+		factor = NumerosPrimos.new
+		factor.calcular(24).should == [2,2,2,3]
+	end
+
+
+	
+end
+
